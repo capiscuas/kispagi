3da5366ea9021dd3817ffc910b14848a8dbb767e
IvanEsperanto <capiscuas@gmail.com>
2018-07-01 16:07:23 +0200

    added usefaircoin and blog projects

--- flask_app/faircoop.py
+++ flask_app/faircoop.py
@@ -8,10 +8,10 @@ areas = [
                                                                                     'faircoop-forum', 'manual-redmine']},
     {'id': 'fairmarket', 'name': 'FairMarket', 'gitlab': 16, 'ocp': [512], 'redmine': ['fairmarket']},
     {'id': 'ocpdev', 'name': 'OCP/Agent Development',  'ocp': [547, 18]},
-    {'id': 'faircoopwebsite', 'name': 'Website/Forum/Wiki/Blog', 'gitlab': 21, 'ocp': [549], 'redmine': ['fair-coop-website']},
+    {'id': 'faircoopwebsite', 'name': 'Website/Forum/Wiki/Blog', 'gitlab': 21, 'ocp': [549], 'redmine': ['fair-coop-website', 'faircoop-blog']},
     {'id': 'kispagi', 'name': 'Kispagi', 'gitlab': 35, 'redmine': ['kispagi']},
     {'id': 'invoices', 'name': 'invoices.freedomcoop.eu', 'gitlab': 34, 'redmine': ['freedomcoop-invoices']},
-    {'id': 'usefaircoin', 'name': 'UseFaircoin', 'gitlab': 13, 'redmine': ['usefaircoin']},
+    {'id': 'usefaircoin', 'name': 'UseFaircoin', 'gitlab': 13, 'redmine': ['usefaircoin'], 'ocp':[1126]},
     {'id': 'coopfunding', 'name': 'CoopFunding', 'gitlab': 80, 'redmine': ['coopfunding']},
     {'id': 'extension', 'name': 'Extension', 'ocp': [713], 'redmine': ['extension']}
 ]
